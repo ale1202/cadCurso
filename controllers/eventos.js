dfc8f357a927d06273e93188ecb9ae9f5be03f17
Alexandre <ale_xandre@ig.com.br>
2018-03-17 22:45:47 -0300

    versao 1.1

--- controllers/eventos.js
+++ controllers/eventos.js
@@ -1,34 +1,63 @@
 module.exports = function (app) {
+    var Evento = app.models.eventos;
+
     var EventosController = {
         menu: function (request, response) {
-        var usuario = request.session.usuario, params = { usuario: usuario };
+            var usuario = request.session.usuario, params = { usuario: usuario };
             response.render('eventos/menu', params);
         },
 
         cadastroUsuarios: function (request, response) {
             var usuario = request.session.usuario, params = { usuario: usuario };
-                response.render('eventos/cadUsuario', params);
+            response.render('eventos/cadUsuario', params);
         },
-        
+
         cadastroCursos: function (request, response) {
             var usuario = request.session.usuario, params = { usuario: usuario };
-                response.render('eventos/cadCursos', params);
+            response.render('eventos/cadCursos', params);
         },
-        
+
+
         listaCursos: function (request, response) {
-            var usuario = request.session.usuario, params = { usuario: usuario };
-                response.render('eventos/listaCursos', params);
+            Cursos.find(function (erro, eventos) {
+                if (erro) {
+                    response.render('/menu');
+                }
+                else {
+                    var usuario = request.session.usuario,
+                        params = { usuario: usuario, cursos: cursos };
+                    response.render('eventos/listaCursos', params);
+                }
+            });
+        },
+    
+        logout: function(request,resposen) {
+            request.session.destroy();
+            response.redirect('/');
         },
 
         //CADASTRO
-       
-        novoCurso: function(request, response) {
+
+        novoCurso: function (request, response) {
             var descricao = request.body.curso.descricao;
             var cargahora = request.body.curso.cargahora;
             var categoria = request.body.curso.categoria;
-        
-            reponse.redirect('/menu')
+
+            if(descricao.trim().length == 0 || cargahora == 'undefined'
+                || categoria.trim().length == 0) {
+                response.redirect('/cadCursos');
+            }
+            else {
+                var curso = request.body.curso;
+                Curso.create(curso, function (erro, curso) {
+                    if (erro) {
+                        response.redirect('/cadCursos');
+                    } else {
+                        response.redirect('/menu');
+                    }
+                });
+            }
         }
-    };
-    return EventosController;
+};
+return EventosController;
 };
--- controllers/home.js
+++ controllers/home.js
@@ -1,37 +1,65 @@
 module.exports = function (app) {
-   var HomeController = {
-       index: function(req, res) {
-           res.render('home/index');
-       },
-       
-       login: function (request, response) {
+
+    var mongoose = require('mongoose');
+    var Usuario = mongoose.model('usuarios');
+
+    var HomeController = {
+        index: function (req, res) {
+            res.render('home/index');
+        },
+
+        login: function (request, response) {
+
             var nome = request.body.usuario.nome;
             var senha = request.body.usuario.senha;
-            
-            if (nome == 'admin' && senha == 'admin') {
-                var usuario = request.body.usuario;
-                request.session.usuario = usuario;
-                response.redirect('/menu');
-            } else {
-                response.redirect('/');
-                }
-            },
-            
+
+            var query = { 'nome': nome, 'senha': senha };
+
+            Usuario.findOne(query).select('nome senha')
+                .exec(function (erro, usuario) {
+                    if (erro) {
+                        response.redirect('/');
+                    } else {
+                        request.session.usuario = usuario;
+                        response.redirect('/menu');
+                    }
+                });
+        },
+        //   if (nome == 'admin' && senha == 'admin') {
+        //       var usuario = request.body.usuario;
+        //       request.session.usuario = usuario;
+        //       response.redirect('/menu');
+        //   } else {
+        //       response.redirect('/');
+        //       }
+
+
         logout: function (request, response) {
             request.session.destroy();
             response.redirect('/');
         },
 
         //CADASTRO
-        
-        novoUsuario: function(request, response) {
+
+        novoUsuario: function (request, response) {
             var nome = request.body.usuario.nome;
             var senha = request.body.usuario.senha;
             var confirma = request.body.usuario.confirma;
 
-            reponse.redirect('/menu')
-        },    
-
-    };        
+            if ((senha != confirma) || nome.trim().length == 0) {
+                response.redirect('/');
+            } else {
+                var usuario = request.body.usuario;
+                Usuario.create(usuario, function (erro, usuario) {
+                    if (erro) {
+                        response.redirect('/');
+                    }
+                    else {
+                        response.redirect('/menu');
+                    }
+                });
+            }
+        }  
+    };  
     return HomeController;
-   };
+};
